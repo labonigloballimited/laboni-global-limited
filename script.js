bd35761ff100b592a5d24dfd046874e1e51ef003
labonigloballimited <labonigloballimited@gmail.com>
2025-10-06 18:27:09 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Simple smooth scroll for navigation links
+document.querySelectorAll('nav ul li a').forEach(link => {
+    link.addEventListener('click', e => {
+        e.preventDefault();
+        const target = document.querySelector(link.getAttribute('href'));
+        target.scrollIntoView({ behavior: 'smooth' });
+    });
+});
